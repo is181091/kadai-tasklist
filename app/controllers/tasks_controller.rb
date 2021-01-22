5b7a66bb3697b3d2073acedeb2dac38a616dacd8
Kota.Yoshizawa <kot-4430@outlook.jp>
2021-01-22 12:32:36 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
+    before_action :require_logged_in?, only: [:index, :show, :edit, :update, :destroy]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
-        @tasks = Task.all.page(params[:page])
+        if logged_in?
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
     def show
        set_task
